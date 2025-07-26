2b0240fe0d2104ff49b3bc9ec1e3710ab220b6c5
Vlad Ferrari <61773649+VladFerrari@users.noreply.github.com>
2025-07-26 23:24:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
-import os
 import telebot
+import os
 from flask import Flask, request
 
 TOKEN = os.environ.get("BOTTOKEN")
 bot = telebot.TeleBot(TOKEN)
-
 app = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def start(message):
     bot.reply_to(message, "Привет! Я бот TicTac Galaxy 🪐")
 
-@app.route(f"/{TOKEN}", methods=['POST'])
-def webhook():
-    json_str = request.get_data().decode('utf-8')
+@app.route(f'/{TOKEN}', methods=['POST'])
+def receive_update():
+    json_str = request.get_data().decode('UTF-8')
     update = telebot.types.Update.de_json(json_str)
     bot.process_new_updates([update])
-    return '', 200
+    return '!', 200
 
-@app.route("/")
-def home():
-    return "✅ TicTac Galaxy Telegram Bot работает."
+@app.route('/')
+def hello():
+    return 'Бот работает!', 200
 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=f"https://tacgalaxy.onrender.com/{TOKEN}")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
